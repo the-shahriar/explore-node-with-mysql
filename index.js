07f9fdd4506ab65abe96397eac90dfe6f46234e4
Shahriar Islam <theshahriarislam@gmail.com>
2021-11-19 18:47:40 +0600

    getting data from mysql database

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
+require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+
+db.connect(function(err) {
+    if (err) {
+      return console.error('Error: ' + err.message);
+    }
+  
+    console.log('Connected to the database.');
+});
 
 // middleware
 app.use(cors());
@@ -11,6 +29,18 @@ app.use(express.json());
 app.get('/', (req, res)=> {
     res.send('Hello world');
     
+})
+
+// getting data from database
+app.get('/userData', (req, res)=> {
+    db.query("SELECT * FROM user_data", (err, result)=> {
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.send(result);
+        }
+    })
 })
 
 app.listen(port, ()=> {
